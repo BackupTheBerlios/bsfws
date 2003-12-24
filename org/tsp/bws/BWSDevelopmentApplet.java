7eade97e6e8b3c75a1c53fab09f8ace7a2bcce01
tobi <tobi>
2003-12-24 14:13:42 +0000

    Added information header, reordered imports, added comments and reformated source

--- org/tsp/bws/BWSDevelopmentApplet.java
+++ org/tsp/bws/BWSDevelopmentApplet.java
@@ -1,67 +1,149 @@
+/******************************************************************************
+ * Class BWSDevelopmentApplet.java
+ * 2003-12-02 by Tobias Specht
+*******************************************************************************
+ * Test applet for Java DOM communication and the JSNode class. Demonstrates
+ * manipulating a in-browser document using JSNode and LiveConnect.
+*******************************************************************************
+ *
+ * Changelog
+ * ---------
+ *
+ * V0.1   @ 2003-12-02
+ *
+*******************************************************************************
+*******************************************************************************
+ *
+ * Licencing Information
+ * ---------------------
+ *
+ * Copyright (C) 2002-2003 Tobias Specht
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * The GNU General Public License is also available on the Web:
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+*******************************************************************************
+ *
+ * Contact information
+ * -------------------
+ *
+ * For further information on this script mail me at:
+ *
+ *     tobi@mail.berlios.de
+ *
+ * The most recent version of this file is available from
+ *
+ *     http://bsfws.berlios.de/
+ *
+******************************************************************************/
+
 package org.tsp.bws;
 
+// import standard java classes
 import java.lang.*;
 import java.applet.*;
-import netscape.javascript.*;
 import java.security.*;
 
-public class bwsDevelopmentApplet extends Applet {
-	public JSObject jsWindow;
-	private JSObject jsDocument;
-	private JSNode currentNode;
+// import liveconnect
+import netscape.javascript.*;
 
-	/** applet constructor */
-	public bwsDevelopmentApplet() {
-		System.out.println("[bwsDevelopmentApplet constructor] applet object created...");
-	}
+public class BWSDevelopmentApplet extends Applet {
+   // browser window the applet is embedded in
+   public JSObject jsWindow;
 
-	/** start method, calls getWindow() */
-	public void start() {
-		jsWindow=JSObject.getWindow(this);
-		System.out.println("[getWindow] window: " + jsWindow.toString());
+   // current document
+   private JSObject jsDocument;
+   
+   // node used for temporary storage
+   private JSNode currentNode;
+   
+   /* debug level,
+    * 0 = don't debug, almost no output
+    * 1 = standard debug
+    */
+   private int debugLevel=1;
+  
+   /** applet constructor */
+   public bwsDevelopmentApplet() {
+      if (debugLevel>0) {
+	  System.out.println("[bwsDevelopmentApplet constructor] applet object created...");
+      }
+   }
 
-		JSObject jsDocument=(JSObject)jsWindow.getMember("document");
-		System.out.println("[getWindow] document: " + jsDocument.toString());
+   /** start method, calls getWindow() */
+   public void start() {
+      // obtain the applets window
+      jsWindow=JSObject.getWindow(this);
+      if (debugLevel > 0) {
+	 System.out.println("[getWindow] window: " + jsWindow.toString());
+      }
+      
+      JSObject jsDocument=(JSObject)jsWindow.getMember("document");
+      if (debugLevel > 0) {
+	 System.out.println("[getWindow] document: " + jsDocument.toString());
+      }
+      
+      JSNode headNode=new JSNode(jsWindow,"testhead");
+      JSNode bodyNode=new JSNode(jsWindow,"theBody");
+      JSNode dieListe=new JSNode(jsWindow,"dieListe");
+      JSNode span=new JSNode(jsWindow,"sp2");
 
-		JSNode headNode=new JSNode(jsWindow,"testhead");
-		JSNode bodyNode=new JSNode(jsWindow,"theBody");
-		JSNode dieListe=new JSNode(jsWindow,"dieListe");
-		JSNode span=new JSNode(jsWindow,"sp2");
+      JSNode newHeading=bodyNode.createElement("h2");
+      JSNode headingText=bodyNode.createTextNode("die zweite überschrift");
 
-		JSNode newHeading=bodyNode.createElement("h2");
-		JSNode headingText=bodyNode.createTextNode("die zweite überschrift");
+      // this is not a debug message! it tests if JSNode.hasChildNodes() works
+      // headingText does not hava any child nodes, answer should be 'false'
+      System.out.println("[main] headingText has child nodes: " + headingText.hasChildNodes());
+      
+      newHeading.appendChild(headingText);
+      
+      bodyNode.insertBefore(newHeading,span);
 
-		System.out.println("[main] headingText has child nodes: " + headingText.hasChildNodes());
+      //bodyNode.appendChild(newHeading);
 
-		newHeading.appendChild(headingText);
+      span.appendData(0,"--append");
+      span.deleteData(0,2,5);
+      dieListe.removeChild(0);
+      dieListe.removeChild(1);
+      
+      // again not a debug message! dieListe should have childNodes -> answer should be 'true'
+      System.out.println("[main] dieListe has child nodes: " + dieListe.hasChildNodes());
+      
+      bodyNode.setAttribute("bgColor","#00ff00");
+      bodyNode.setStyleAttribute("color","red");
+      headNode.setStyleAttribute("backgroundColor","#88ff88");
+      headNode.setStyleAttribute("border","1px solid #555555");
+      bodyNode.setStyleAttribute("fontFamily","verdana");
+      
+      // two more test (=not debug) messages
+      System.out.println("[main] body - color attribute: " + bodyNode.getAttribute("bgColor"));
+      System.out.println("[main] body - color attribute node: " + bodyNode.getAttributeNode("bgColor").getNode().getMember("value"));
+      
+      headNode.replaceChild("Replaced",0);
+      headNode.replaceData(2,7,"---");
+      
+      headNode.insertData(0,2,"(TEST)");
+   }
 
-		bodyNode.insertBefore(newHeading,span);
-
-		//bodyNode.appendChild(newHeading);
-
-		span.appendData(0,"--append");
-		span.deleteData(0,2,5);
-		dieListe.removeChild(0);
-		dieListe.removeChild(1);
-		System.out.println("[main] dieListe has child nodes: " + dieListe.hasChildNodes());
-
-		bodyNode.setAttribute("bgColor","#00ff00");
-		bodyNode.setStyleAttribute("color","red");
-		headNode.setStyleAttribute("backgroundColor","#88ff88");
-		headNode.setStyleAttribute("border","1px solid #555555");
-		bodyNode.setStyleAttribute("fontFamily","verdana");
-		System.out.println("[main] body - color attribute: " + bodyNode.getAttribute("bgColor"));
-		System.out.println("[main] body - color attribute node: " + bodyNode.getAttributeNode("bgColor").getNode().getMember("value"));
-
-		headNode.replaceChild("Replaced",0);
-		headNode.replaceData(2,7,"---");
-
-		headNode.insertData(0,2,"(TEST)");
-	}
-
-	/** obtains the document window and references it in jsWindow */
-	public void getWindow() {
-		jsWindow=JSObject.getWindow(this);
-		System.out.println("[getWindow] window: " + jsWindow.toString());
-	}
+   /** obtains the document window and references it in jsWindow */
+   public void getWindow() {
+      jsWindow=JSObject.getWindow(this);
+      if (debugLevel>0) {
+	 System.out.println("[getWindow] window: " + jsWindow.toString());
+      }
+   }
 }
